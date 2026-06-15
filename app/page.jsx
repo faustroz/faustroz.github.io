b69de0751f8712ca162e60f4412a00cea91643a6
faustr0z <ferdysuper171@gmail.com>
2026-06-15 18:10:58 +0700

    Delete labvalues section

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/hero";
 import About from "@/components/about";
-import LabValues from "@/components/lab-values";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50">
       <Hero />
       <About />
-      <LabValues />
     </main>
   );
 }
